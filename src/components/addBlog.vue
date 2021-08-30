bd300f7b662bf32942687fb0e84af6f47bbcca97
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-30 13:21:08 -0400

    part 29

---
+++ src/components/addBlog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id='add-blog'>
+    <h2>Add a new blog post</h2> 
+    <form>
+      <label>Blog title</label>
+      <input type='text' v-model.lazy='blog.title' required />
+      <label>Blog Content</label>
+      <textarea v-model.lazy='blog.content'></textarea>
+    </form> 
+    <div id='preview'>
+      <h3>Preview Blog</h3>
+      <p>Blog title: {{ blog.title }}</p>
+      <p>Blog content:</p>
+      <p>{{ blog.content }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+ 
+  data () {
+    return {
+      blog: {
+        title: '',
+        content: ''
+      } 
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
