6f02fc74d0edf866491e8b6ace5cb033a16ad6d4
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-30 14:23:43 -0400

    part 33

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id='show-blogs'>
+    <h1>All Blog Articles</h1>
+    <div class='single-blog' v-for='blog in blogs'>
+      <h2>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      blogs: []
+    }
+  },
+  methods: {
+   
+  },
+   created() {
+      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
+        this.blogs = data.body.slice(0, 10);
+      })
+    }
+}
+</script>
+
+<style>
+
+</style>
