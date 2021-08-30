daa39af520f9c4f8fd86ec180ccd20a028557d46
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-30 08:51:46 -0400

    part 20

--- src/ninjas.vue
+++ src/ninjas.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul>
-    <li v-for='ninja in ninjas'>{{ ninja }}</li>
-  </ul>
+  <div>
+    <h1>List of ninjas</h1>
+    <ul>
+      <li v-for='ninja in ninjas'>{{ ninja }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -16,7 +19,8 @@ export default {
 </script>
 
 <style>
-
-
+h1 {
+    color: green;
+}
 
 </style>
