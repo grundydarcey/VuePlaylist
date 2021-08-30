12e0ae74e4e6356200c9ba13ecf130c69142cb38
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-30 15:43:15 -0400

    part 41

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to='/' exact>Blog</router-link></li>
+      <li><router-link to='/add' exact>Add new Blog</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default{}
+</script>
+
+<style scoped>
+</style>
