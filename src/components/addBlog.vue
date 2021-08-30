0bf5f5c6b7a75aa0019085f31038bae1dcd5819e
Darcey Grundy <grundy.darcey@gmail.com>
2021-08-30 13:28:49 -0400

    part 30

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -6,12 +6,26 @@
       <input type='text' v-model.lazy='blog.title' required />
       <label>Blog Content</label>
       <textarea v-model.lazy='blog.content'></textarea>
+      <div id='checkboxes'>
+        <label>Ninjas</label>
+        <input type='checkbox' value='ninjas' v-model='blog.categories' />
+        <label>qizards</label>
+        <input type='checkbox' value='qizards' v-model='blog.categories' />
+        <label>witches</label>
+        <input type='checkbox' value='witches' v-model='blog.categories' />
+        <label>cheese</label>
+        <input type='checkbox' value='cheese' v-model='blog.categories' />
+      </div>
     </form> 
     <div id='preview'>
       <h3>Preview Blog</h3>
       <p>Blog title: {{ blog.title }}</p>
       <p>Blog content:</p>
       <p>{{ blog.content }}</p>
+      <p>Blog categories</p>
+      <ul>
+        <li v-for='category in blog.categories'>{{ category }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -24,7 +38,8 @@ export default {
     return {
       blog: {
         title: '',
-        content: ''
+        content: '',
+        categories: []
       } 
     }
   },
